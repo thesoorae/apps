2118b895bcdc5a62fa5e8b9854dcd0532e5dd376
thesoorae <the.soorae@gmail.com>
2015-07-08 20:56:54 -0700

    final version with better layout

--- animation/animation/ViewController.swift
+++ animation/animation/ViewController.swift
@@ -11,14 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     var timer=NSTimer()
-    var count=1
+    var count=0
+    var isAnimating=false
 
     @IBOutlet var pugImage: UIImageView!
     
+    @IBOutlet var why: UILabel!
    
     @IBAction func explode(sender: AnyObject) {
         timer.invalidate()
-    pugImage.image=UIImage(named:"sadpug.jpg")}
+    pugImage.image=UIImage(named:"sadpug.jpg")
+   why.text="whyyyyyyyyy???" }
     
     func result(){
         if count == 3 {count = 1}
@@ -26,10 +29,12 @@ class ViewController: UIViewController {
         pugImage.image = UIImage(named:"frame\(count).png")}
     
     @IBAction func updateImage(sender: AnyObject) {
-        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
-
-        
-    }
+        why.text=" "
+        if isAnimating == false {
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
+        isAnimating=true} else {timer.invalidate()
+            isAnimating = false}}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -40,14 +45,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+/*
     override func viewDidLayoutSubviews() {
         pugImage.frame = CGRectMake(100, 20, 0, 0)
     }
     override func viewDidAppear(animated:Bool){
         UIView.animateWithDuration(1, animations: {() -> Void in
             self.pugImage.frame = CGRectMake(100, 20, 100,200) })}
-    
+    */
     }
 
 
