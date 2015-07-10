bc5d7c4ecaf3d9ce67d879183fc87da9f1e9bb4b
thesoorae <the.soorae@gmail.com>
2015-07-10 12:18:45 -0700

    segues from table view to view with record of row selected

---
+++ advanced segues/advanced segues/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  advanced segues
+//
+//  Created by the Soo-Rae's Mac on 7/10/15.
+//  Copyright (c) 2015 sourceapps. All rights reserved.
+//
+
+import UIKit
+
+var rowCounter:Int = 0
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        println(rowCounter)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
