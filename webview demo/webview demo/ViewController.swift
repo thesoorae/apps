ac3297cfd66d7b282b360ea58538df72161d8f17
thesoorae <the.soorae@gmail.com>
2015-07-16 06:16:12 -0700

    just loading webview

---
+++ webview demo/webview demo/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  webview demo
+//
+//  Created by the Soo-Rae's Mac on 7/16/15.
+//  Copyright (c) 2015 sourceapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var webview: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        var url = NSURL(string: "http://www.google.com")
+        var request = NSURLRequest(URL:url!)
+        webview.loadRequest(request)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
