589547aab4cd2261a0ae778cd6ee7243d980282b
thesoorae <the.soorae@gmail.com>
2015-08-06 10:10:20 -0700

    tinder clone part 1, setting up login with Facebook . for Xcode 7

--- shroomies/shroomies/GameScene.swift
+++ shroomies/shroomies/GameScene.swift
@@ -11,19 +11,19 @@ import SpriteKit
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var eater = SKSpriteNode()
-    var porcini = SKSpriteNode()
-    var chant = SKSpriteNode()
+    var shroom = SKSpriteNode()
+
     var tree = SKSpriteNode()
-    var movingObjects = SKSpriteNode()
+    var movingObjects = SKNode()
     
-    var eaterGroup : UInt32 = 1
-    var shroomGroup : UInt32 = 2
-    var objectGroup : UInt32 = 3
+    var eaterGroup : UInt32 = 0x1 << 1
+    var objectGroup : UInt32 = 0x1 << 2
+
+    var shroomGroup : UInt32 = 0x1 << 3
     
     var score = 0
     var scoreLabel = SKLabelNode()
-    var moverSpeed = 10.0
-    let moveFactor = 1.05
+
     
     var points = 0
     
@@ -34,6 +34,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
         /* Setup your scene here */
         
+        self.physicsWorld.contactDelegate = self
+        self.physicsWorld.gravity = CGVectorMake(0,-2)
+        self.addChild(movingObjects)
         
         scoreLabel.fontName = "Helvetica"
         scoreLabel.fontSize = 60
@@ -41,13 +44,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         scoreLabel.position = CGPointMake(CGRectGetMidY(self.frame), (self.frame.size.height-70))
         self.addChild(scoreLabel)
         
-        
-        self.physicsWorld.contactDelegate = self
-        self.physicsWorld.gravity = CGVectorMake(0,-5)
-        self.addChild(movingObjects)
+        self.backgroundColor = SKColor.greenColor()
+      //  self.scaleMode = SKSceneScaleMode.AspectFit
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect:self.frame)
+    
 
-    
-    
+        
+        
+        
         var eaterTexture = SKTexture(imageNamed: "pacman")
         var eaterTexture2 = SKTexture(imageNamed: "pacman-open")
         var animation = SKAction.animateWithTextures([eaterTexture, eaterTexture2], timePerFrame: 0.2)
@@ -59,8 +63,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         eater.physicsBody = SKPhysicsBody(circleOfRadius: eater.size.height/2)
         eater.physicsBody?.dynamic = true
-        eater.physicsBody?.allowsRotation = true
-        eater.zPosition = 10
+        eater.physicsBody?.allowsRotation = false
+        // eater.zPosition = 10
         eater.physicsBody?.categoryBitMask = eaterGroup
         eater.physicsBody?.collisionBitMask = objectGroup
         eater.physicsBody?.contactTestBitMask = objectGroup
@@ -69,29 +73,35 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.addChild(eater)
         
         var ground = SKNode()
-        ground.position = CGPointMake(0,0)
+        ground.position = CGPointMake(0, 20)
         ground.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(self.frame.size.width, 1))
-        ground.physicsBody?.dynamic = false
+        ground.physicsBody!.dynamic = false
         ground.physicsBody?.categoryBitMask = objectGroup
         self.addChild(ground)
-        var timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("makeShrooms"), userInfo: nil, repeats: true)
+        
+        
+        var timer = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: Selector("makeShrooms"), userInfo: nil, repeats: true)
     }
     
     func makeShrooms(){
         if gameOver == 0 {
-            
-            
-            var newY = Int(arc4random()%1024)
+            var height1: Int = Int(self.frame.height)
+            var height2 = UInt32(height1)
+            var newY = Int(arc4random_uniform(height2))
             var newX = Int(self.frame.width + 10)
             var p = CGPoint(x:newX, y:newY)
             var destination = CGPoint(x:0, y:newY)
             
-            createShroom(p, destination:destination)
+            createShroom(p)
             
         
         }}
         
-        func createShroom(p:CGPoint, destination:CGPoint){
+        func createShroom(p:CGPoint){
+            var moveShroom = SKAction.moveByX(-self.frame.size.width * 2, y:0, duration: NSTimeInterval(self.frame.size.width/100))
+            var removeShroom = SKAction.removeFromParent()
+            var moveAndRemoveShroom = SKAction.sequence([moveShroom, removeShroom])
+            
 var whichShroom = String()
             var x = Int(arc4random()%2)
             if x == 0 {whichShroom = "chanterelle"
@@ -100,22 +110,22 @@ var whichShroom = String()
             points = 200}
             let shroom = SKSpriteNode(imageNamed: whichShroom)
         shroom.position = p
+            shroom.runAction(moveAndRemoveShroom)
+            shroom.physicsBody = SKPhysicsBody(rectangleOfSize: shroom.size)
+            shroom.physicsBody!.dynamic = false
             shroom.physicsBody?.categoryBitMask = shroomGroup
-            let duration = NSTimeInterval(moverSpeed)
-            let action = SKAction.moveTo(destination, duration: duration)
-            shroom.runAction(SKAction.repeatActionForever(action))
+
+            
             movingObjects.addChild(shroom)
         }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         /* Called when a touch begins */
-        
-        if gameOver == 0 {
-            eater.physicsBody?.velocity = CGVectorMake(0,0)
-            eater.physicsBody?.applyImpulse(CGVectorMake(0, 50))
+            eater.physicsBody!.velocity = CGVectorMake(100,100)
+            eater.physicsBody!.applyImpulse(CGVectorMake(20, 300)
+)
 
-        }
-        }
+                }
     
     func didBeginContact(contact: SKPhysicsContact) {
         if contact.bodyA.categoryBitMask == shroomGroup || contact.bodyB.categoryBitMask == shroomGroup
